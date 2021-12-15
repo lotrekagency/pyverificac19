562ef3ffc8fa9a889c5bab0b7097e0b947a69372
Mirko Bernardini <mirko@lotrek.it>
2021-12-15 22:20:50 +0100

    feat: Creare un metodo per catalogare il DCC #11 DONE

--- examples/spike_verifier.py
+++ examples/spike_verifier.py
@@ -1,5 +1,18 @@
 from verificac19.verifier import Verifier
-
+from dcc_utils import from_image
 verifier = Verifier()
 
-print(verifier.verify_image('tests/data/eu_test_certificates/SK_1.png'))
+
+file_test = {
+    'vaccine' : 'tests/data/eu_test_certificates/SK_1.png',
+    'not_valide_cert' : 'tests/data/not_valid_certificate.png',
+    'invalid' : 'tests/data/invalid.png',
+    'test' : 'tests/data/eu_test_certificates/TEST.png',
+    'sm' : 'tests/data/eu_test_certificates/SM_1.png',
+}
+print(verifier.verify_image(file_test['sm']))
+
+
+dcc_not_sm = from_image(file_test['sm'])
+dcc_not_sm._payload['v'][-1]['co'] = 'IT'
+print(verifier._check_vaccination(dcc_not_sm.payload))
--- verificac19/verifier.py
+++ verificac19/verifier.py
@@ -1,15 +1,68 @@
 from dcc_utils import from_image, from_raw
+from dcc_utils.exceptions import DCCParsingError
 
-super_gp_mode = "2G"
+SUPER_GP_MODE = "2G"
+
+NOT_EU_DCC = 'NOT_EU_DCC'
+NOT_VALID = 'NOT_VALID'
+NOT_VALID_YET = 'NOT_VALID_YET'
+VALID = 'VALID'
 
 class Verifier():
 
-    @staticmethod
-    def verify_image(path, super_gp_mode=super_gp_mode):
-        mydcc = from_image(path)
-        return mydcc
+    @classmethod
+    def _check_vaccination(cls, payload): 
+        print('Vaccino')
+        print(payload)
+        if payload['v'][-1]["mp"] == "Sputnik-V" and payload['v'][-1]["co"] != "SM":
+            return {
+                "code": NOT_VALID,
+                "result": False,
+                "message" : 'Vaccine Sputnik-V is valid only in San Marino',
+            }
 
-    @staticmethod
-    def verify_raw(raw,super_gp_mode=super_gp_mode):
+    @classmethod
+    def _check_test(cls, payload): 
+        print('Test')
+        print(payload)
+
+    @classmethod
+    def _check_recovery(cls, payload): 
+        print('Ricovero')
+        print(payload)
+
+    @classmethod
+    def _verify(cls, dcc, super_gp_mode):
+        payload = dcc.payload
+        if 'v' in payload:
+            result = cls._check_vaccination(payload)
+        elif 't' in payload:
+            if super_gp_mode == SUPER_GP_MODE:
+                return {
+                        "code": NOT_VALID,
+                        "result": False,
+                        "message" : 'Certificate is not valid',
+                    }
+            result = cls._check_test(payload)
+        elif 'r' in payload:
+            result = cls._check_recovery(payload)
+        else: print('schianto')
+        return result
+
+    @classmethod
+    def verify_image(cls, path, super_gp_mode=SUPER_GP_MODE):
+        try:
+            mydcc = from_image(path)
+            response = cls._verify(mydcc, super_gp_mode)
+            return response
+        except DCCParsingError:
+            return {
+                    "code": NOT_VALID,
+                    "result": False,
+                    "message" : 'Certificate is not valid',
+                }
+
+    @classmethod
+    def verify_raw(cls, raw,super_gp_mode=SUPER_GP_MODE):
         mydcc = from_raw(raw)
         return mydcc
