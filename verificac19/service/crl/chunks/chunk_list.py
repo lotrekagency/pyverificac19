19c9ac07b50b8b4666147de1bc9bd50c1048b048
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-01-18 18:26:47 +0100

    feat(service): created chunk list class

--- verificac19/service/crl/chunks/chunk_list.py
+++ verificac19/service/crl/chunks/chunk_list.py
@@ -1,3 +1,25 @@
+from .chunk import Chunk
 
 class ChunkList:
-    pass
+
+    def __init__(self, first_chunk: Chunk=None) -> None:
+        self._first_chunk = first_chunk
+        self._last_chunk = self._first_chunk
+
+    def __iter__(self):
+        return self._first_chunk
+
+    def add_chunk(self, new_chunk: Chunk):
+        if self.is_empty():
+            self.__init__(new_chunk)
+            return
+
+        self._last_chunk.set_next_chunk(new_chunk)
+        self._last_chunk = new_chunk
+
+
+    def is_empty(self):
+        if self._first_chunk is None:
+            return True
+
+        return False
