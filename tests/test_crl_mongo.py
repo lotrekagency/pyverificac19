1efab8f0875d23e933215ae344fb3a14dcce6722
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-01-20 04:42:16 +0100

    fix(test): corrected import error in test

--- tests/test_crl_mongo.py
+++ tests/test_crl_mongo.py
@@ -5,15 +5,15 @@ crl = MongoCRL()
 
 
 def test_mongo_crl_insert_and_deletions():
-    crl.store_revoked_uvci(["a", "b"], ["b"])
+    crl.update_crl(["a", "b"], ["b"])
     assert crl.is_uvci_revoked("a")
     crl.clean_all()
     assert not crl.is_uvci_revoked("a")
 
 
 def test_mongo_crl_multiple_insert():
-    crl.store_revoked_uvci(["a", "b"], ["b"])
-    crl.store_revoked_uvci(["a", "b", "c"])
+    crl.update_crl(["a", "b"], ["b"])
+    crl.update_crl(["a", "b", "c"])
     assert crl.is_uvci_revoked("a")
     assert crl.is_uvci_revoked("b")
     assert crl.is_uvci_revoked("c")
