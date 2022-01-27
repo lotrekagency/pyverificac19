767821d537baf9ca7bb1d56a5e64ce3114e2e3ed
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-01-27 17:59:23 +0100

    chore(service): added utils.py

---
+++ tests/service/utils.py
@@ -0,0 +1,7 @@
+import os
+import json
+
+def open_json(name: str) -> dict:
+    path = os.path.join("tests", "data", "mock_request", name)
+    with open(path, "r") as file:
+        return json.load(file)
