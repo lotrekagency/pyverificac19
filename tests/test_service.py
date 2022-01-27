47b4f549942223ba2fe36f22c114ce7220517eea
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-01-27 16:00:02 +0100

    chore(service): added testing of settings and dsc

--- tests/test_service.py
+++ tests/test_service.py
@@ -17,15 +17,61 @@ class TestService:
         with open(path, "r") as file:
             return json.load(file)
 
-    @pook.on
-    def test_dsc_settings(self):
+    def setup_mock_for_settings(self):
         settings_data = self.open_json("settings.json")
         pook.get(SETTINGS_URL, reply=200, response_json=settings_data)
+
+    def setup_mock_for_dsc_certificates(self):
+        FINISHED = 'finished'
+        pook.get(STATUS_URL, reply=200, response_json=self.open_json("dsc_whitelist.json"))
+        dsc_validation: list = self.open_json("dsc_validation.json")
+        dsc_validation.append(FINISHED)
+        for index, dsc in enumerate(dsc_validation):
+            kwargs = {}
+            headers = {"content-type": "text/plain"}
+            kwargs['reply'] = 200
+            if 0 < index:
+                headers["X-RESUME-TOKEN"] = str(index)
+            kwargs['headers'] = headers
+            if dsc == FINISHED:
+                kwargs['reply'] = 204
+                pook.get(DSC_URL, **kwargs)
+                continue
+
+            kwargs['response_body'] = dsc["raw_data"]
+            kwargs['response_headers'] = {
+                "X-KID": dsc["kid"],
+                "X-RESUME-TOKEN": str(index + 1),
+            }
+            pook.get(DSC_URL, **kwargs)
+
+    @pook.on
+    def test_dsc_settings(self):
+        self.setup_mock_for_settings()
+        self.setup_mock_for_dsc_certificates()
         service.update_settings()
+        settings_data = self.open_json("settings.json")
         for setting in settings_data:
             st_name = setting['name']
             st_type = setting['type']
             assert service.get_setting(st_name, st_type) == setting
+
+    @pook.on
+    def test_dsc_certificates(self):
+        self.setup_mock_for_dsc_certificates()
+
+        not_whitelisted_dsc = ["nJkLGpmXT68=", "b32660V8q5A=", "gBG2e8Vypv0="]
+        service.update_dsc()
+        dsc_list = self.open_json("dsc_validation.json")
+        for dsc in dsc_list:
+            kid = dsc['kid']
+            data = dsc['raw_data']
+            stored_dsc = service.get_dsc(kid)
+            if kid in not_whitelisted_dsc:
+                assert stored_dsc is None
+                continue
+
+            assert stored_dsc == data
 
     @pook.on
     def test_update_all(self):
