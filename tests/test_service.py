736d3e26439ea584d668a6728b4130deaccea530
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-01-27 16:38:18 +0100

    chore(service): cleaned up service testing

--- tests/test_service.py
+++ tests/test_service.py
@@ -45,60 +45,7 @@ class TestService:
             }
             pook.get(DSC_URL, **kwargs)
 
-    @pook.on
-    def test_dsc_settings(self):
-        self.setup_mock_for_settings()
-        self.setup_mock_for_dsc_certificates()
-        service.update_settings()
-        settings_data = self.open_json("settings.json")
-        for setting in settings_data:
-            st_name = setting['name']
-            st_type = setting['type']
-            assert service.get_setting(st_name, st_type) == setting
-
-    @pook.on
-    def test_dsc_certificates(self):
-        self.setup_mock_for_dsc_certificates()
-
-        not_whitelisted_dsc = ["nJkLGpmXT68=", "b32660V8q5A=", "gBG2e8Vypv0="]
-        service.update_dsc()
-        dsc_list = self.open_json("dsc_validation.json")
-        for dsc in dsc_list:
-            kid = dsc['kid']
-            data = dsc['raw_data']
-            stored_dsc = service.get_dsc(kid)
-            if kid in not_whitelisted_dsc:
-                assert stored_dsc is None
-                continue
-
-            assert stored_dsc == data
-
-    @pook.on
-    def test_update_all(self):
-        pook.get(SETTINGS_URL, reply=200, response_json=self.open_json("settings.json"))
-        pook.get(STATUS_URL, reply=200, response_json=self.open_json("certificate_status.json"))
-        dsc_validation = self.open_json("dsc_validation.json")
-        for index, dsc in enumerate(dsc_validation):
-            header = {"content-type": "text/plain"}
-            if index > 0:
-                header["X-RESUME-TOKEN"] = str(index)
-
-            reply = 200 if index < len(dsc_validation) - 1 else 204
-            response_headers = {
-                "X-KID": dsc["kid"],
-                "X-RESUME-TOKEN": str(index + 1),
-            }
-
-            pook.get(
-                DSC_URL,
-                reply=reply,
-                response_body=dsc["raw_data"],
-                headers=header,
-                response_headers=response_headers,
-            )
-
-        service.clear_all_cache()
-
+    def setup_mock_for_crl(self):
         pook.get(CHECK_CRL_URL, reply=200, response_json=self.open_json("CRL-check-v1.json"))
         pook.get(
             f"{DOWNLOAD_CRL_URL}?chunk=1",
@@ -111,4 +58,32 @@ class TestService:
             response_json=self.open_json("CRL-v1-c2.json"),
         )
 
+    @pook.on
+    def test_setup(self):
+        self.setup_mock_for_settings()
+        self.setup_mock_for_dsc_certificates()
+        self.setup_mock_for_crl()
+        service.clear_all_cache()
         service.update_all()
+
+    @pook.on
+    def test_dsc_settings(self):
+        settings_data = self.open_json("settings.json")
+        for setting in settings_data:
+            st_name = setting['name']
+            st_type = setting['type']
+            assert service.get_setting(st_name, st_type) == setting
+
+    @pook.on
+    def test_dsc_certificates(self):
+        not_whitelisted_dsc = ["nJkLGpmXT68=", "b32660V8q5A=", "gBG2e8Vypv0="]
+        dsc_list = self.open_json("dsc_validation.json")
+        for dsc in dsc_list:
+            kid = dsc['kid']
+            data = dsc['raw_data']
+            stored_dsc = service.get_dsc(kid)
+
+            if kid in not_whitelisted_dsc:
+                assert stored_dsc is None
+            else:
+                assert stored_dsc == data
