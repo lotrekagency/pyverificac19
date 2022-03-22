e53fb24854fba471ec5f9ecf98c7256dbd15923b
Federico Di Filitto <federico.difilitto@protonmail.com>
2022-03-22 15:08:26 +0100

    feat(verifier): improved datetime validator

--- verificac19/verifier/verifier_types/utils/time.py
+++ verificac19/verifier/verifier_types/utils/time.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timedelta
 from enum import Enum
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Timing(Enum):
     TOO_EARLY = -1
@@ -15,9 +18,27 @@ class DateTimeValidator:
         self.now = datetime.now(reference_date.tzinfo)
 
     def check(self) -> Timing:
+        logger.debug(f"Checking between {self.start_time} and {self.end_time}. Now is {self.now}")
         if self.now < self.start_time:
             return Timing.TOO_EARLY
         elif self.now > self.end_time:
             return Timing.TOO_LATE
 
         return Timing.VALID
+
+    @property
+    def start_time_string(self) -> str:
+        return self.start_time.strftime("%Y-%m-%d %H:%M:%S%z")
+
+    @property
+    def start_date_string(self) -> str:
+        return self.start_time.strftime("%Y-%m-%d")
+
+
+    @property
+    def end_time_string(self) -> str:
+        return self.end_time.strftime("%Y-%m-%d %H:%M:%S%z")
+
+    @property
+    def end_date_string(self) -> str:
+        return self.end_time.strftime("%Y-%m-%d")
